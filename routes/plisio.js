294b48e1e79e88919f2b131ceab50d7863fa69a2
betonssm <betonssm@gmail.com>
2025-04-17 21:29:20 +0200

    описание коммита

--- routes/plisio.js
+++ routes/plisio.js
@@ -16,8 +16,8 @@ router.post("/create-payment", async (req, res) => {
         order_name: "MMM GO Premium",
         order_number: telegramId,
         callback_url: "https://mmmgo-backend.onrender.com/plisio/callback",
-        success_url: "https://mmmgo-game.web.app/payment-success",
-        cancel_url: "https://mmmgo-game.web.app/payment-failed",
+        success_url: "https://mmmgo-game.web.app/PaymentSuccess",
+        cancel_url: "https://mmmgo-game.web.app/PaymentFailed",
       },
       headers: {
         Authorization: `Bearer ${process.env.PLISIO_API_KEY}`,
